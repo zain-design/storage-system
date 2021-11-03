21418bcb7d1ac8cb790169d386b22ae244921fb8
psychohaxer <apclead69@gmail.com>
2021-11-03 17:23:57 +0700

    adding homepage, undone

---
+++ homepage.php
@@ -0,0 +1,50 @@
+<?php
+	
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Sistem Arsip Tata Usaha </title>
+        <link rel="stylesheet" type="text/css" href="../style/style.css">
+        <link rel="stylesheet" type="text/css" href="../style/responsive.css">
+        <link rel="stylesheet" type="text/css" href="../style/imgsrc.css">
+        <link rel="stylesheet" type="text/css" href="../style/home.css">
+        <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+        <style ="text/css">
+            body {
+                background-image:url("../img/bg.jpg");
+                background-repeat:no-repeat;
+                background-position:center;
+                background-attachment:fixed;
+                background-size:100%;
+            }
+        </style>
+    </head>
+    <body>
+        <hr id=homepageline>
+        <hr id=homepageline>
+        <table border="0px" width="100%">
+            <tr>
+                <td width="100%" height="120px" id="judul"> <center> Storage System </center> </td>
+            </tr>
+        </table>
+        <hr id=homepageline>
+        <hr id=homepageline>
+        <nav id="header">
+            <ul id="navbar">
+            <a href="login.php">
+                <li>&#9749; Login</li>
+            </a>
+                <li>&#10029; Information
+                    <ul>
+                        <a href="help.php"><li> &#10067; Help </li></a>
+                    </ul>
+                    <ul>
+                        <a href="about.php"><li> &#10067; About </li></a>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </body>
+</html>
