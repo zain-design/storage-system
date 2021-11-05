2d1d7a369f0e617ce71dd2b71c135cf61f0235a9
zain-design <66899058+zain-design@users.noreply.github.com>
2021-11-05 13:05:00 +0700

    Create help.php

---
+++ help.php
@@ -0,0 +1,15 @@
+<?php
+    include "components/home_header.php";
+?>
+
+<br><br><br><br>
+<form method="POST" action="../operation/ceklogin.php">
+			<div class="container" style="color:black" id="helpcont">
+			<h1 id="deftext">web operating instructions<br/></h1>	
+            <p> </p>
+            <p id="deftext">- users are available in 2 types, admin and user</p>
+            <p id="deftext">- user = admin & pass = admin</p>
+            <p id="deftext">- user = user & pass = user</p>
+			</div>
+		</form>
+    
