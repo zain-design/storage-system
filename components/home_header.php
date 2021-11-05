b4470f8b4f0171de80001e413dfa31fa63798057
psychohaxer <apclead69@gmail.com>
2021-11-05 10:34:25 +0700

    add homepage header and global footer

---
+++ components/home_header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Storage System </title>
+        <link rel="stylesheet" type="text/css" href="style/style.css">
+        <link rel="stylesheet" type="text/css" href="style/home.css">
+        <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+        <style ="text/css">
+            body {
+                background-image:url("img/bg.png");
+                background-repeat:no-repeat;
+                background-position:center;
+                background-attachment:fixed;
+                background-size:100%;
+            }
+        </style>
+    </head>
+    <body>
+        <nav id="header">
+            <ul id="navbar">
+                <a href="homepage.php"> <li>&#10063; Storage System</li> </a>   
+                <a href="login.php"> <li>&#9749; Login</li> </a>
+                <a href="help.php"><li> &#10067; Help </li></a>
+                <a href="about.php"><li> &#9734; About </li></a>
+            </ul>
+        </nav>
